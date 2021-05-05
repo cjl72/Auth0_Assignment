01b91ae11ca35937785196c8594dc9e24dddfb7c
cjl72 <cjl72@njit.edu>
2021-05-05 16:11:27 -0400

    Task: Updated App Configuration

--- index.js
+++ index.js
@@ -73,6 +73,14 @@ passport.use(strategy);
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
 /**
  * Routes Definitions
  */
